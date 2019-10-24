dd7f9cc655be9e9cd64706fc7ae2a431b54e01f6
melodyabikhalil <melody.ak@hotmail.com>
2019-10-25 00:18:37 +0300

    Adds functioning code2

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def db_connection(user,password,host,port,database):
         cursor = connection.cursor()
         cursor.execute("SELECT version();")
         record = cursor.fetchone()
+        print(record[0])
         return connection
     except (Exception, psycopg2.Error) as error :
         print ("Error while connecting to PostgreSQL", error)
@@ -23,11 +24,11 @@ def db_connection(user,password,host,port,database):
 def db_close(connection):
     connection.cursor().close()
     connection.close()
-    print("PostgreSQL connection is closed")
+    print("DB connection is closed")
 
 def get_columns(source_conx, table_name):
     cursor = source_conx.cursor()
-    select_query = "SELECT * FROM "+table_name+" WHERE 1=1"
+    select_query = "SELECT * FROM "+table_name+" WHERE 1=2"
     cursor.execute(select_query)
     columns = [i[0] for i in cursor.description]
     return columns
@@ -50,14 +51,16 @@ def select_query_pk(source_conx, source_table, child_fk, parent_record):
     return records
 
 def insert_update_query(record, target_conx, target_table, pk, columns):
+    pk_index = columns.index(pk)
+    pk_value = record[pk_index]
     cursor = target_conx.cursor()
-    select_query = "SELECT * FROM "+target_table+" WHERE "+pk+" = '"+str(record[0])+"'"
+    select_query = "SELECT * FROM "+target_table+" WHERE "+pk+" = '"+str(pk_value)+"'"
     query = ""
     cursor.execute(select_query)
     if (cursor.rowcount <= 0):
         query = prepare_insert_query(target_table, record, columns)
     else:
-        query = prepare_update_query(target_table, record, pk, record[0], columns)
+        query = prepare_update_query(target_table, record, pk, pk_value, columns)
     cursor.execute(query)
     target_conx.commit()
     
@@ -99,7 +102,8 @@ def prepare_update_query(table_name, record, pk_name, pk_value, columns):
                     j+=2
                 else:
                     j+=1
-            query+= str(columns[i])+" = '"+str(value)+"' ,"
+            if(str(columns[i]) != pk_name):
+                query+= str(columns[i])+" = '"+str(value)+"' ,"
         i+=1
     query=query[:len(query)-1] 
     query +=" WHERE "+pk_name+" = '"+str(pk_value)+"'"
@@ -142,16 +146,23 @@ def main():
     for table in json_database_info["tables"]:
         now = get_current_datetime(source_conx)
         table_name = table["name"]
-        records = select_query_timestamp(source_conx, table_name, table["timestamp"], json_last_runtimes[table_name], now) 
+ 
+        records = select_query_timestamp(source_conx, table_name, table["timestamp"], json_last_runtimes[table_name], now)
+        # Initial call to print 0% progress
+        l = len(records) + 1
+        printProgressBar(1, l, prefix = table_name+':', suffix = 'records', decimals = 0, length = 50)
         columns = get_columns(source_conx, table_name)
         table["columns"] = columns 
+        i = 0
         for record in records:
+            i = i + 1
+            printProgressBar(i+1, l, prefix = table_name+':', suffix = 'records', decimals = 0, length = 50)
             insert_update_query(record, target_conx, table['target_table'], table["pk"], columns)
             for child in table["children"]:
                 child_records = select_query_pk(source_conx, child["name"], child["fk"], record)
                 child_columns = get_columns(source_conx, child["target_table"])
                 for child_record in child_records:
-                    insert_update_query(child_record, target_conx, child['target_table'], child["pk"], child_columns)
+                    insert_update_query(child_record, target_conx, child["target_table"], child["pk"], child_columns)
         json_last_runtimes[table_name] = now
         write_file("./database_info.json", json_database_info)
         write_file("./last_runtimes.json", json_last_runtimes)
@@ -159,5 +170,26 @@ def main():
     db_close(source_conx)
     db_close(target_conx)
         
-    
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix + ' --> ' + str(iteration)), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 main()
